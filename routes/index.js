8c207a9c9f5556b509b569c3cef56a81c89401b1
Michael <michael.saj@gmail.com>
2016-05-20 01:05:01 -0400

    Created toto_list route and aggregated multiple collections

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var mongodb = require('mongodb');
 
+
 // Variable declaration
 var db_data;        
 var redirect_url;
@@ -13,15 +14,16 @@ var MongoClient = mongodb.MongoClient;
 var url = 'mongodb://localhost:27017/assignment_db';
 
 
-
 /******************************* GET METHODS *******************************/
 
-/* GET home page. */
+/* 
+Route: index
+GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* To-do List Page 
+/* Route: thelist
 Get list from DB
 */
 router.get('/thelist', function(req,res) {
@@ -40,9 +42,7 @@ router.get('/thelist', function(req,res) {
           res.send(err);
         }
         else if (result.length){
-          res.render('users_list',{
-            "userslist":result
-          });
+          res.render('users_list', { "userslist":result });
         }
         else{
           res.send('No documents found');
@@ -53,27 +53,85 @@ router.get('/thelist', function(req,res) {
   })
 });
 
-/* Routing get request for '/todo_list' link route */
+/* Route: todo_list
+Routing get request for '/todo_list' link route */
 router.get('/todo_list',function (req,res) {
-  res.render('user_todo_list', {title: 'Todo List'});
+  res.render('new_todo', {title: 'Todo List'});
 });
 
-/* Routing get request for '/newuser' link route */
+/* Route: newuser
+Routing get request for '/newuser' link route */
 router.get('/newuser',function (req,res) {
   res.render('newuser', {title: 'Add User'});
 });
-/******************************* END OF GET METHODS *******************************/
 
+/* Route: users_list
+Routing get request for '/users_list' link route */
+router.get('/final',function (req,res) {
+
+  MongoClient.connect(url, function(err,db) {
+   
+    if(err){
+      console.log('Unable to connect to the server');
+   
+   }
+    else{
+      console.log('Connection Established');
+      
+      var collection = db.collection('todo_list');
+      //var collection = db.collection('users');
+      
+collection.aggregate([{
+  $lookup:{
+    from: "users",
+    localField: "_id",
+    foreignField: "_id",
+    as: "other"    
+   }
+  }],function(err, result){
+    //console.log(" Rice "+result.length);
+    //console.dir(result);
+  //});
+      
+      
+      
+      //collection.find({}).toArray(function(err, result){
+
+        if (err){
+          res.send(err);
+        }
+        else if (result.length){
+          
+          //console.log("data: " + result);
+          
+          res.render('todo_list', { "record":result });
+        }
+        else{
+          res.send('No documents found');
+        }
+        db.close();
+      });
+    }
+  })
+  
+  //res.render('users_list', {title: 'users list'});
+
+});
+
+
+
+/******************************* END OF GET METHODS *******************************/
 
 
 /******************************* POST METHODS *******************************/
 
-/* Add_list
+/* Route: Add_list
 Routing post request for '/add_list' link route */
 router.post('/add_list', function(req, res) {
 
   /* Json data */
   db_data = { 
+    _id: req.body._id, 
     title: req.body.title, 
     location:req.body.location, 
     notes:req.body.notes, 
@@ -81,11 +139,11 @@ router.post('/add_list', function(req, res) {
         
   redirect_url = "final";
   collection_name = "todo_list";
-  insert_into_db(db_data, collection_name, redirect_url, req, res);
+  insert_into_db(req, res);
   
 });
 
-/* Add User
+/* Route: Add User
 Routing post request for '/adduser' link route */
 router.post('/adduser', function(req, res) {
 
@@ -97,7 +155,7 @@ router.post('/adduser', function(req, res) {
         
   redirect_url = "todo_list";
   collection_name = "users";
-  insert_into_db(db_data, collection_name, redirect_url, req, res);
+  insert_into_db(req, res);
   
 });
 
@@ -105,7 +163,7 @@ router.post('/adduser', function(req, res) {
 
 
 /******************************* INSERT TO DB FUNCTION *******************************/
-function insert_into_db(db_data, collection_name, redirect_url, req, res)
+function insert_into_db(req, res)
 {
   //MongoDB connection function
   MongoClient.connect(url, function(err,db) {  
